dd47cabef2305e3860f66883a407cde78ac62ea9
FatimaGaykhan <fatimaeg@code.edu.az>
2024-04-14 22:40:28 +0400

    Added Seed Data

--- Repository/Data/AppDbContext.cs
+++ Repository/Data/AppDbContext.cs
@@ -15,6 +15,24 @@ namespace Repository.Data
         {
             optionsBuilder.UseSqlServer("Server=localhost,1433;Database=EFCourseApp;User Id=sa;Password=Salam123");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Education>().HasData(
+                new Education { Id=1000,Name="Back End",Color="orange",CreatedDate=DateTime.Now},
+                new Education { Id=1001,Name="Front End",Color="green", CreatedDate=DateTime.Now},
+                new Education { Id=1002,Name="Cyber Security", Color="black", CreatedDate=DateTime.Now}
+                );
+            modelBuilder.Entity<Group>().HasData(
+                new Group { Id=30,Name="Group100",Capacity=20,EducationId=1000, CreatedDate=DateTime.Now},
+                new Group { Id = 32, Name = "Group105", Capacity = 15, EducationId = 1001, CreatedDate = DateTime.Now },
+                new Group { Id = 33, Name = "Group106", Capacity = 20, EducationId = 1002, CreatedDate = DateTime.Now },
+                new Group { Id = 34, Name = "Group107", Capacity = 18, EducationId = 1002, CreatedDate = DateTime.Now }
+                );
+
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
 
---
+++ Repository/Data/Migrations/20240414183636_SeedData.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Educations",
+                columns: new[] { "Id", "Color", "CreatedDate", "Name" },
+                values: new object[] { 1000, "orange", new DateTime(2024, 4, 14, 22, 36, 36, 515, DateTimeKind.Local).AddTicks(1180), "Back End" });
+
+            migrationBuilder.InsertData(
+                table: "Educations",
+                columns: new[] { "Id", "Color", "CreatedDate", "Name" },
+                values: new object[] { 1001, "green", new DateTime(2024, 4, 14, 22, 36, 36, 515, DateTimeKind.Local).AddTicks(1210), "Front End" });
+
+            migrationBuilder.InsertData(
+                table: "Educations",
+                columns: new[] { "Id", "Color", "CreatedDate", "Name" },
+                values: new object[] { 1002, "black", new DateTime(2024, 4, 14, 22, 36, 36, 515, DateTimeKind.Local).AddTicks(1220), "Cyber Security" });
+
+            migrationBuilder.InsertData(
+                table: "Groups",
+                columns: new[] { "Id", "Capacity", "CreatedDate", "EducationId", "Name" },
+                values: new object[,]
+                {
+                    { 30, 20, new DateTime(2024, 4, 14, 22, 36, 36, 515, DateTimeKind.Local).AddTicks(1270), 1000, "Group100" },
+                    { 32, 15, new DateTime(2024, 4, 14, 22, 36, 36, 515, DateTimeKind.Local).AddTicks(1280), 1001, "Group105" },
+                    { 33, 20, new DateTime(2024, 4, 14, 22, 36, 36, 515, DateTimeKind.Local).AddTicks(1280), 1002, "Group106" },
+                    { 34, 18, new DateTime(2024, 4, 14, 22, 36, 36, 515, DateTimeKind.Local).AddTicks(1280), 1002, "Group107" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 32);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 33);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 34);
+
+            migrationBuilder.DeleteData(
+                table: "Educations",
+                keyColumn: "Id",
+                keyValue: 1000);
+
+            migrationBuilder.DeleteData(
+                table: "Educations",
+                keyColumn: "Id",
+                keyValue: 1001);
+
+            migrationBuilder.DeleteData(
+                table: "Educations",
+                keyColumn: "Id",
+                keyValue: 1002);
+        }
+    }
+}
